fbc31b9fc7867653cfa18ccd099fef53f6e4978b
abdo <aboabdo8600@gmail.com>
2024-02-05 20:46:01 +0200

    first

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
-    // Toggle menu for small screens
     $("#menuIcon").click(function () {
         $("#menuDropdown").toggleClass("show");
     });
 
-    // Toggle profile dropdown
     $("#profileIcon").click(function () {
         $("#profileDropdown").toggle();
     });
 
-    // Close menu dropdown when clicking outside
     $(document).click(function (event) {
         if (!event.target.closest('#menuIcon')) {
             $("#menuDropdown").removeClass("show");
         }
     });
     
-    // Close profile dropdown when clicking outside
     $(document).click(function (event) {
         if (!event.target.closest('#profileIcon')) {
             $("#profileDropdown").hide();
@@ -26,7 +22,29 @@ $(document).ready(function () {
 
 
 
-// profile
+// mode
+
+// $(document).ready(function () {
+//     const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+//     setMode(isDarkMode);
+
+//     $('#darkModeToggle').change(function () {
+//         const darkModeEnabled = $(this).prop('checked');
+//         setMode(darkModeEnabled);
+//     });
+
+//     function setMode(enableDarkMode) {
+//         if (enableDarkMode) {
+//             $('body').addClass('dark-mode');
+//             localStorage.setItem('darkMode', 'enabled');
+//         } else {
+//             $('body').removeClass('dark-mode');
+//             localStorage.setItem('darkMode', null);
+//         }
+//     }
+// });
+
 
 $(document).ready(function () {
     const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
@@ -44,7 +62,14 @@ $(document).ready(function () {
             localStorage.setItem('darkMode', 'enabled');
         } else {
             $('body').removeClass('dark-mode');
-            localStorage.setItem('darkMode', null);
+            localStorage.setItem('darkMode', 'disabled');
         }
     }
+
+    // إعادة تحميل حالة الدارك مود عند تحميل الصفحة
+    window.onload = function () {
+        const isDarkModeOnLoad = localStorage.getItem('darkMode') === 'enabled';
+        $('#darkModeToggle').prop('checked', isDarkModeOnLoad);
+        setMode(isDarkModeOnLoad);
+    };
 });
